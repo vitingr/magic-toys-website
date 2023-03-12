0f05d9c66a738a853c7a14c2dedf92a750d08d85
vitingr <vitorgabrielsbo1460@gmail.com>
2023-03-12 00:34:51 -0300

    Update Responsividade

--- scripts.js
+++ scripts.js
@@ -5,3 +5,11 @@ window.addEventListener("scroll", () => {
     header.classList.toggle("sticky", this.window.scrollY > 0)
 
 })
+
+let menu = document.querySelector("#menu-icon")
+let navmenu = document.querySelector(".navmenu")
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x")
+    navmenu.classList.toggle("open")
+}
